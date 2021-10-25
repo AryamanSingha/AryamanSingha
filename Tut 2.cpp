1051532543ba20f8ed6f15c1c3032a3e02ee9a0b
AryamanSingha <93110066+AryamanSingha@users.noreply.github.com>
2021-10-25 14:23:15 +0530

    Create Tut 2.cpp

---
+++ Tut 2.cpp
@@ -0,0 +1,16 @@
+- 👋 Hi, I’m @AryamanSingha
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+AryamanSingha/AryamanSingha is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+#include<iostream>
+Using namespace std;
+Int main(){
+Cout<<"hello Ayan ";
+Return 0;
+}
